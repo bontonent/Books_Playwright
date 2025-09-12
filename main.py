e938a25d4f8526ac2ff2697199ff769aa1cb09da
bontonent <bontonent@tuta.io>
2025-09-12 12:50:06 +0300

    already have all data. Next step. maybe async, and try understood how work async.(a lot of question)

--- main.py
+++ main.py
@@ -28,24 +28,61 @@ class PlayWrightManager:
         pd = sync_playwright().start()
         website = pd.chromium.launch(headless=True)
 
-        # Work with catalog
-        page_catalog = website.new_page()
-        page_catalog.goto(self.url)
-        self.get_url_page(page_catalog)
+        # uncomment for use get url
+        # # Work with catalog
+        # page_catalog = website.new_page()
+        # page_catalog.goto(self.url)
+        # self.get_url_page(page_catalog)
 
         #Start work with product page
         page_product = website.new_page()
         # I don't know how
         # wait new url element
-        for one_url in self.base_url:
-            page_product.goto(one_url)
 
+        # for one_url in self.base_url:
+        one_url = 'https://books.toscrape.com/catalogue/1000-places-to-see-before-you-die_1/index.html'
+        page_product.goto(one_url)
+        self.get_data_product_page(page_product)
 
-        print(self.base_url)
-        print(len(self.base_url))
+        # uncomment for use get url
+        # print(self.base_url)
+        # print(len(self.base_url))
         website.close()
 
-
+# for work with product page
+    def get_data_product_page(self,page_product):
+        # get all necessary data
+        for index_gen,genre_el in enumerate(page_product.query_selector_all("ul.breadcrumb li a")):
+            if index_gen == 2:
+                genre = genre_el.text_content()
+        for title_el in page_product.query_selector_all("div h1"):
+            title = title_el.text_content()
+        for index_e, right_elements in enumerate(page_product.query_selector_all("div.row div p")):
+            if index_e == 0:
+                price = right_elements.text_content()
+            if index_e == 1:
+                stock = right_elements.text_content().replace("\n","").strip()
+            if index_e == 2:
+                stars = right_elements.get_attribute("class")
+        for describe_el in page_product.query_selector_all("article.product_page > p"):
+            describe = describe_el.text_content()
+        for img_el in page_product.query_selector_all("div.item.active img"):
+            img_url = "".join(["https://books.toscrape.com/",img_el.get_attribute('src').replace("../../","")])
+        for table_el in page_product.query_selector_all("tbody"):
+            for th_table_el,td_table_el in zip(table_el.query_selector_all("th"),table_el.query_selector_all("td")):
+                if th_table_el.text_content() == "UPC":
+                    UPC = td_table_el.text_content()
+                if th_table_el.text_content() == "Product Type":
+                    Product_Type = td_table_el.text_content()
+                if th_table_el.text_content() == "Price (excl. tax)":
+                    Price_excl_tax = td_table_el.text_content()
+                if th_table_el.text_content() == "Price (incl. tax)":
+                    Price_incl_tax = td_table_el.text_content()
+                if th_table_el.text_content() == "Tax":
+                    Tax = td_table_el.text_content()
+                # I don't need in stock available, I had
+                if th_table_el.text_content() == "Number of reviews":
+                    Number_of_reviews = td_table_el.text_content()
 
 # for work with catalog
     def get_url_page(self,page_catalog):
@@ -77,23 +114,3 @@ if __name__ == "__main__":
     init_class.main()
 
     print("End class")
-
-
-#     page_get_url = browser.new_page()
-#     page_get_url.goto("https://books.toscrape.com/catalogue/category/books_1/index.html")
-#
-#     array_url = []
-#     for page_text in page_get_url.query_selector_all("h3 a"):
-#         array_url.append("".join(["https://books.toscrape.com/catalogue/",page_text.get_attribute("href").replace("../../","")]))
-#
-#     print(array_url,len(array_url))
-#
-#     page_get_url = browser.new_page()
-#     # element wait url
-#     if array_url == None:
-# if __name__ == "__main__":
-#     manager = PlaywrightManager()
-#     manager.navigate_to('https://example.com')
-#     print('Page Title:', manager.get_page_title())
-#     manager.close()
-#
