140aac651e75d1492b70d0494eac123edd433ffc
bontonent <bontonent@tuta.io>
2025-09-12 09:41:37 +0300

    try how work class. And search better decision for work with 3 process

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Or tree columns go or tree page : 3 process
+#-------------------------------------------------
+# tree columns conveniently with breakdown process
+# tree page conveniently work with 3 process
+#
+# one process get link
+# two process open site, close site
+
 from playwright.sync_api import sync_playwright
 
 # def create_new_page(browser,url):
@@ -7,19 +15,48 @@ from playwright.sync_api import sync_playwright
 #         product_page.wait_for_timeout(300)
 #         print(element_page.text_content())
 
-with sync_playwright() as p:
+# main Process
+class PlayWrightManager:
+    def __init__(self):
+        print("Start project")
 
-    browser = p.chromium.launch()
-    page = browser.new_page()
-    page.goto("https://books.toscrape.com/catalogue/category/books_1/index.html")
-    page.query_selector("h3 a").click()
-    page.wait_for_timeout(10)
-    for page_element in page.query_selector_all("body div div div div article.product_page div.row div p.price_color"):
-        print(page_element.text_content())
-    # for page_text in page.query_selector_all("h3 a"):
-    #     new_url = "".join(["https://books.toscrape.com/catalogue/",page_text.get_attribute("href").replace("../../","")])
+    def main_directory(self):
+        pd = sync_playwright().start()
 
-        # create_new_page(browser,new_url)
+        website = pd.chromium.launch(headless=True)
+        page = website.new_page()
+        url = "https://books.toscrape.com/catalogue/category/books_1/index.html"
+        page.goto(url)
+        page.screenshot(path = "example.png")
+        website.close()
 
-    browser.close()
 
+if __name__ == "__main__":
+    init_class = PlayWrightManager()
+    init_class.main_directory()
+
+
+#     page_get_url = browser.new_page()
+#     page_get_url.goto("https://books.toscrape.com/catalogue/category/books_1/index.html")
+#
+#     array_url = []
+#     for page_text in page_get_url.query_selector_all("h3 a"):
+#         array_url.append("".join(["https://books.toscrape.com/catalogue/",page_text.get_attribute("href").replace("../../","")]))
+#
+#     print(array_url,len(array_url))
+#
+#     page_get_url = browser.new_page()
+#     # element wait url
+#     if array_url == None:
+#
+#
+#
+#
+#     browser.close()
+#
+# if __name__ == "__main__":
+#     manager = PlaywrightManager()
+#     manager.navigate_to('https://example.com')
+#     print('Page Title:', manager.get_page_title())
+#     manager.close()
+#
