76a074946ef09c43ada720c308a22017efdb27a9
bontonent <bontonent@tuta.io>
2025-09-12 11:07:48 +0300

    Work get url in array. Only get data. And program element complete.(if don't say about sql and async, sync)

--- main.py
+++ main.py
@@ -17,23 +17,54 @@ from playwright.sync_api import sync_playwright
 
 # main Process
 class PlayWrightManager:
-    def __init__(self):
+    def __init__(self,url):
+        self.url = url
+        self.base_url = [] # save all url, what don't work on it
         print("Start project")
+        self.i = 0
 
-    def main_directory(self):
+    def main(self):
         pd = sync_playwright().start()
 
         website = pd.chromium.launch(headless=True)
         page = website.new_page()
-        url = "https://books.toscrape.com/catalogue/category/books_1/index.html"
-        page.goto(url)
-        page.screenshot(path = "example.png")
+        page.goto(self.url)
+        self.get_url_page(page)
+        print(self.base_url)
+        print(len(self.base_url))
         website.close()
 
+    def get_url_page(self,page):
+
+        still_work = True
+        while (still_work):
+            for page_text in page.query_selector_all("h3 a"):
+                self.base_url.append("".join(["https://books.toscrape.com/catalogue/",page_text.get_attribute("href").replace("../../","")]))
+
+            still_work = self.next_page(page)
+            # print(still_work)
+
+    def next_page(self,page):
+        # try click on new page
+        try:
+            for button_next_page in page.query_selector_all("section div div ul li a[href]"):
+                # print(button_next_page.text_content())
+                if button_next_page.text_content() == "next":
+                    self.i = self.i + 1
+                    print(self.i)
+                    button_next_page.click()
+                    page.wait_for_timeout(10) # don't need view png in the site/ for png 1000 need
+                    return True
+            return False
+        except:
+            return False
 
 if __name__ == "__main__":
-    init_class = PlayWrightManager()
-    init_class.main_directory()
+    url = "https://books.toscrape.com/catalogue/category/books_1/index.html"
+    init_class = PlayWrightManager(url)
+    init_class.main()
+
+    print("End class")
 
 
 #     page_get_url = browser.new_page()
@@ -48,12 +79,6 @@ if __name__ == "__main__":
 #     page_get_url = browser.new_page()
 #     # element wait url
 #     if array_url == None:
-#
-#
-#
-#
-#     browser.close()
-#
 # if __name__ == "__main__":
 #     manager = PlaywrightManager()
 #     manager.navigate_to('https://example.com')
